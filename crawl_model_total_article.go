f76c86ca797a4199116d1c220033a795abaf913c
wangxinglong.wxl <wangxinglong.wxl@bytedance.com>
2026-01-30 11:32:49 +0800

    update

--- crawl_model_total_article.go
+++
@@ -1,8 +0,0 @@
-package go_figshare
-
-type TotalArticle struct {
-	Views     int64 `json:"views"`
-	Downloads int64 `json:"downloads"`
-	Shares    int64 `json:"shares"`
-	Cites     int64 `json:"cites"`
-}
